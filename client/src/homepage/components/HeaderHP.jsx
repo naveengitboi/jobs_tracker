6d5c10bd6bfffd76e7256d3e51ee51b02bf2b684
naveengitboi <naveenjangiti9@gmail.com>
2025-05-30 23:51:42 +0530

    done homepage navbar

--- client/src/homepage/components/HeaderHP.jsx
+++ client/src/homepage/components/HeaderHP.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import icons from '../../assets/icons'
+import "../../styles/components/Navbar.css"
 
 function HeaderHP() {
-  return (
-    <div>HeaderHP</div>
-  )
+    return (
+        <div className="navbarContainer headerHpNavbarWrapper">
+            <div className="navListItems headerHpNavListItems">
+                <div className="navbarHeader">
+                    <p className="mdTxt">Job Tracker</p>
+                </div>
+                <ul className="regTxt">
+                    <li><NavLink to={"/"}> Home</NavLink></li>
+                    <li><NavLink to={"applications"}>Features</NavLink></li>
+                    <li><NavLink to="templates">Templates</NavLink></li>
+                </ul>
+            </div>
+            <div className="headerHpActions">
+                <NavLink to="profile" className={""}>
+                    Sign Up
+                </NavLink>
+                <NavLink to="profile">
+                    Log In
+                </NavLink>
+            </div>
+        </div>
+    )
 }
 
 export default HeaderHP
